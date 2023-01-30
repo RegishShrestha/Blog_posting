c506ba25cd57ce2f96ec457f2509e2d62a1b6c9d
RegishShrestha <regishshrestha@gmail.com>
2023-01-30 09:35:08 +0545

    mongoose and mongodb connected

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
 const app = express();
-app.set("view engine", "ejs");
+const Blog = require("./models/blog");
 
-app.listen(3000);
+// connect to mongoDB
+const dbURI =
+  "mongodb+srv://netninja:1234@nodetuts.gxyk5lo.mongodb.net/node-tuts?retryWrites=true&w=majority";
+mongoose
+  .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    app.listen(3000);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// register view engine
+app.set("view engine", "ejs");
 
 // these are some middlewares
 // app.use((req, res, next) => {
@@ -24,6 +38,36 @@ app.use(express.static("public"));
 
 app.use(morgan("dev"));
 
+/*
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "new blog",
+    snippet: "about my new blog",
+    body: "more about my new blog",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/all-blog", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+*/
+
+/*
 app.get("/", (req, res) => {
   const blogs = [
     {
@@ -43,8 +87,27 @@ app.get("/", (req, res) => {
   res.render("index", { title: "home", blogs });
 });
 
+*/
+
+app.get("/", (req, res) => {
+  res.redirect("/blog");
+});
+
 app.get("/about", (req, res) => {
   res.render("about", { title: "about" });
+});
+
+// blogs
+
+app.get("/blog", (req, res) => {
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("index", { title: "All blogs", blogs: result });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.get("/blog/create", (req, res) => {
