675c7f613062ba9c2ac2d4878d525c4158bed083
RegishShrestha <regishshrestha@gmail.com>
2023-01-30 20:40:46 +0545

    id and delete added

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Blog = require("./models/blog");
 
 // connect to mongoDB
 const dbURI =
-  "mongodb+srv://<username>:<password>@nodetuts.gxyk5lo.mongodb.net/node-tuts?retryWrites=true&w=majority";
+  "mongodb+srv://usernaame:password@nodetuts.gxyk5lo.mongodb.net/node-tuts?retryWrites=true&w=majority";
 mongoose
   .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then((result) => {
@@ -35,7 +35,7 @@ app.set("view engine", "ejs");
 
 // middleware & static files
 app.use(express.static("public"));
-
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 
 /*
@@ -90,7 +90,7 @@ app.get("/", (req, res) => {
 */
 
 app.get("/", (req, res) => {
-  res.redirect("/blog");
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
@@ -99,7 +99,7 @@ app.get("/about", (req, res) => {
 
 // blogs
 
-app.get("/blog", (req, res) => {
+app.get("/blogs", (req, res) => {
   Blog.find()
     .sort({ createdAt: -1 })
     .then((result) => {
@@ -110,9 +110,33 @@ app.get("/blog", (req, res) => {
     });
 });
 
-app.get("/blog/create", (req, res) => {
+app.post("/blogs", (req, res) => {
+  const blog = new Blog(req.body);
+  blog
+    .save()
+    .then((result) => {
+      res.redirect("/blogs");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/blogs/:id", (req, res) => {
+  const id = req.params.id;
+  Blog.findById(id)
+    .then((result) => {
+      res.render("details", { blog: result, title: "Blog Details" });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.get("/blogs/create", (req, res) => {
   res.render("create", { title: "create" });
 });
+
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
